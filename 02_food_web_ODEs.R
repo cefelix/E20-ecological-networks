21657da3e8a6a71a01f4ddeb26a92f78bd2f7490
cefelix <felix.zeh@gmx.de>
2023-04-28 06:25:26 +0200

    prey/predator counts for each species added

--- 02_food_web_ODEs.R
+++ 02_food_web_ODEs.R
@@ -12,16 +12,16 @@ library(ggplot2)
 ###
 ####2.1 set up parameters and food web####
 #number of basal species depends on the food web!
-n_tot <- 128              #no. of all species
+n_tot <- 64              #no. of all species
 
 perc_sub <- 0.25          #share of the species present in sub-food webs (float between 0 and 1)
 n_sub <- perc_sub*n_tot   #no. of species in a sub-foodweb
 
-con <- seq(0.025, 0.05, by = 0.00125)   #connectance of the food webs 
+con <- seq(0.025, 0.05, by = 0.0125)   #connectance of the food webs 
                                         #check out https://www.pnas.org/doi/epdf/10.1073/pnas.192407699
                                         #mininmal connectance 0.026, maximal 0.315 (from 17 empirical food webs, fig.1)
 
-reps<- 5                                #no of replicates per food web
+reps<- 2                                #no of replicates per food web
 times <- seq(1, 1e12, by = 1e9)         #time for integration of dynamics
 biom <- runif(n_tot, 1, 4)              #initial biomasses
 BM <- runif(n_tot, 1, 12) %>%           #Body masses 
@@ -50,7 +50,21 @@ extinction_array <- provideDimnames(extinction_array, sep = "_", base = list("re
 troph.lvl_array <- array(dim = c(reps,length(con), n_tot))
 troph.lvl_array <- provideDimnames(troph.lvl_array, sep = "_", base = list("rep", "con", "spec"))
 
+#prey at start (for each species)
+prey_array <- array(dim = c(reps,length(con), n_tot))
+prey_array <- provideDimnames(prey_array, sep = "_", base = list("rep", "con", "spec"))
 
+#predators at start
+predators_array <- array(dim = c(reps,length(con), n_tot))
+predators_array <- provideDimnames(predators_array, sep = "_", base = list("rep", "con", "spec"))
+
+#prey at end
+prey_array.end <- array(dim = c(reps,length(con), n_tot))
+prey_array.end <- provideDimnames(prey_array.end, sep = "_", base = list("rep", "con", "spec"))
+
+#predators at end
+predators_array.end <- array(dim = c(reps,length(con), n_tot))
+predators_array.end <- provideDimnames(predators_array.end, sep = "_", base = list("rep", "con", "spec"))
 
 #loop counters (i set them up against the alphabet because that's cooler)
 j=1
@@ -91,10 +105,38 @@ for (j in 1:reps) {
     #trophic levels 
     troph.lvl_array[j,i,] = TroLev(fw)
     
+    #preys
+    prey_array[j,i,] = colSums(fw)
+    
+    #predators
+    predators_array[j,i,] = rowSums(fw)
+    
+    #preys at end
+    prey_array.end[j,i,] = colSums(fw) * (+(!exts))
+          #the second term takes the extinction vector, flips 0 for 1 and vice versa 
+          #and multiplies it by the initial sum of prey species for each concerning species
+          #this removes interactions with extinct species
+    prey_array.end[j,i,] = prey_array.end[j,i,]*(+(!exts))
+      #this deletes the species, which are extinct themselves
+    
+    #predators at end
+    predators_array[j,i,] = rowSums(fw) * (+(!exts))
+    predators_array[j,i,] = predators_array[j,i,] * (+(!exts))
+    
   }
   
   print(j)
 }
+
+#AND THIS HAS A BUG! (still considers extinct species)
+#links (pred/prey) at end of simulation
+prey_array.end <- prey_array * +(!extinction_array) 
+  #this first inverses the extinction array, so that a 0 means extinct and a 1 means present
+  #then the prey_array (binary, 0 and 1) gets multiplied by the inversed extinction array
+  #so each interaction which involves an extinct species gets set to zero
+
+predators_array.end <- predators_array * +(!extinction_array)
+
 
 
 ###
