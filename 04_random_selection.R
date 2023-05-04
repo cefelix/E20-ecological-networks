d2e8b8b984640108421ac2817c44d35dc9a60b30
cefelix <felix.zeh@gmx.de>
2023-05-04 16:01:05 +0200

    integrated this into 03_data_prep.R

--- 04_random_selection.R
+++ 04_random_selection.R
@@ -1,24 +1,2 @@
-#select randomly but based on conditions
-library(ATNr)
-
-
-#read data (output of code #3)
-d <- read.csv("./data/64spec_2000randoms.csv")[,-1] #drops first column which is rownumber
-
-#read RDS files (output of code #2): (DELETE ?! - irrelevant)
-abundance_array <- readRDS(file = "./raw/abundance.rds")
-biomass_array <- readRDS(file = "./raw/biomass.rds")
-extinction_array <-  readRDS(file = "./raw/extinction.rds")
-troph.lvl_array <- readRDS(file = "./raw/trophic_lvl.rds")
-
-
-####
-str(d)
-
-####extinctions####
-#condition = 
-
-while (condition) {
-  
-}
+#integrated into 03_data_prep
 
