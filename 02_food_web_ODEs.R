76fd9765ebadcc1ba25ce9de9b1ed693aa691035
cefelix <felix.zeh@gmx.de>
2023-05-08 23:11:56 +0200

    edited commenting

--- 02_food_web_ODEs.R
+++ 02_food_web_ODEs.R
@@ -21,14 +21,11 @@ set.seed(666)
 #con <- seq(0.025, 0.05, by = 0.025)   #connectance of the food webs 
                                         #check out https://www.pnas.org/doi/epdf/10.1073/pnas.192407699
                                         #mininmal connectance 0.026, maximal 0.315 (from 17 empirical food webs, fig.1)
-con <- runif(15, min=0.05, max=0.35)
-con <- sort(con)
+con <- runif(100, min=0.05, max=0.35) %>% sort()
+
 
 times <- seq(1, 1e12, by = 1e9)         #time for integration of dynamics
 biom <- runif(n_tot, 1, 4)              #initial biomasses
-BM <- runif(n_tot, 1, 12) %>%           #Body masses 
-  sort()                                #realistic spanning range in soil food webs (Potapov 2021):
-BM <- (10^BM)                           #https://pubmed.ncbi.nlm.nih.gov/34086977/
 ext_thresh <- 0.1**6                    #threshold below which species is considered, extinct
 
 
@@ -76,10 +73,6 @@ i=1
 ####2.3 compute 1e10 food webs####
   for (i in 1:length(con)){
     
-    BM <- runif(n_tot, 1, 12) %>% #body masses of the species
-      sort()
-    BM <- (10^BM)
-    
     fw <- create_niche_model(S = n_tot, C = con[i])
     n_bas <- sum(colSums(fw) == 0)
     
@@ -87,9 +80,12 @@ i=1
       fw <- create_niche_model(S = n_tot, C = con[i])
       n_bas <- sum(colSums(fw) == 0)
     }
-  
     
-    model <- create_model_Unscaled(n_tot, n_bas, BM, fw) %>%
+    BM <- runif(n_tot, 1, 12) %>% 
+      sort()
+    BM <- (10^BM) #Body masses, spanning 12 orders of magnitude
+    
+    model <- create_model_Unscaled(n_tot, n_bas, BM, fw) #%>%
       initialise_default_Unscaled()
     model$ext <- ext_thresh
     
@@ -143,7 +139,7 @@ output_96 <- list(abundance_array, biomass_array, extinction_array, troph.lvl_ar
                   prey_array, prey_array.end, predators_array, predators_array.end)
 names(output_96) <- c("abundances", "biomasses", "extinctions", "troph_lvl", 
                       "feed_on_START", "feed_on_END", "consumed_by_START", "consumed_by_END")
-saveRDS(output_96, file = "./raw/20220505_96spec_15cons_v01.rds")
+saveRDS(output_96, file = "./raw/20220508_96spec_0100c_04to12bas_v01.rds")
 
 
 ###
