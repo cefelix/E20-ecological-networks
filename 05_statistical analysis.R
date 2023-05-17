8f3e6d68c51566d0a33791071f8b48bb03e35670
cefelix <felix.zeh@gmx.de>
2023-05-17 08:05:16 +0200

    cleared from old exploratory scatterplots

--- 05_statistical analysis.R
+++ 05_statistical analysis.R
@@ -1,48 +1,6 @@
 #statistical analysis
 library(ATNr)
 
-data <- read.csv("./data/96spec_2000randoms.csv")[,-1] #drops first column which is rownumber
-d <- data
+d <- read.csv("./data/20220506_96spec_2000c_04to12bas_v02.csv")[,-1] #drops first column which is rownumber
 
-d_real <- subset(d, d$con <= 0.30)
-d <- d_real
-####scatterplots extinctions####
-
-plot(d$con, d$n_basal) #WARNING: increasing connectance reduces number of basal species
-
-plot(d$maxTL ,d$n_basal, 
-     xlim = c(0,40)) #less basal species produce higher maximal trophic levels 
-
-plot(d$con, d$ext_all)
-  plot(d$ext_big, d$ext_all)
-  plot(d$ext_small, d$ext_all)
-plot(d$con, d$ext_big)
-plot(d$con, d$ext_small)
-plot(d$con, d$ext_rand)
-
-####scatterplots shannon####
-plot(d$con, d$shan_all)
-  plot(d$shan_big, d$shan_all)
-  plot(d$shan_small, d$shan_all)
-plot(d$con, d$shan_big)
-plot(d$con, d$shan_small)
-
-
-plot(d$shan_all, d$ext_all)
-plot(d$shan_all, d$con)
-plot(d$con, d$shan_all)
-
-plot(d$con, d$shan_rand)
-
-plot(d$con, d$shan_BAS)
-
-####scatterplots trophic level####
-plot(d$con, d$minTL_B) 
-summary(d$minTL_s)
-plot(d$con, d$shan_small)
-
-log(32)#
-log(64)
-log(16)
-log(128) #maximum shannon index 
 
